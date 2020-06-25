ab8c8937822301a812f6892684ba919abed36f26
Marza <Marza@DESKTOP-62B6RDV>
2020-06-25 13:52:16 +0700

    Tugas Struktur Data

---
+++ src/tugassd/tugassd2.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugassd;
+
+import java.util.*;
+
+public class tugassd2 {
+    
+public static void main(String args[])
+{
+// Creating an empty Stack
+Stack<Integer> STACK = new Stack<Integer>();
+// Use push() to add elements into the Stack
+STACK.push(10);
+STACK.push(15);
+STACK.push(30);
+STACK.push(20);
+STACK.push(5);
+// Displaying the Stack
+System.out.println("Stack Awal: " + STACK);
+// Pushing elements into the Stack
+STACK.push(1254);
+STACK.push(4521);
+// Displaying the final Stack
+System.out.println("Update Stack: " + STACK);
+}
+}
+
